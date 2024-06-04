95d682eb4e358bed2e3f1822be7f108d51c7ffa9
Your Name <your.email@example.com>
2024-06-04 23:30:12 +0530

    hari

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('v1').then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/styles.css',
+          '/script.js',
+          '/manifest.json',
+          '/path/to/icon-192x192.png',
+          '/path/to/icon-512x512.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
